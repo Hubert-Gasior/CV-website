06ffa8c400a2c43568b6895ad5d43f98d12cbefb
Hubert Gasior <hgasior7@gmail.com>
2024-08-09 11:30:45 +0200

    CV finished

--- CV/home_page/urls.py
+++ CV/home_page/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('home_page/', views.home_page, name='home_page'),
     path('home_page/exp_page/', views.exp_page, name='exp_page'),
     path('home_page/hobby_page/', views.hobby_page, name='hobby_page'),
-    path('home_page/qualities_page/', views.qualities_page, name='qualities_page')
+    path('home_page/qualities_page/', views.qualities_page, name='qualities_page'),
+    path('home_page/exp_page/hobby_page/', views.hobby_page, name='hobby_page'),
+    path('home_page/exp_page/qualities_page/', views.qualities_page, name='qualities_page')
 ]
