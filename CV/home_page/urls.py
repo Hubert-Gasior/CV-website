3d82c834ad1ec3c1a7b5cff210dc5020f1f3375b
Hubert Gasior <hgasior7@gmail.com>
2024-09-08 18:08:05 +0200

    version 1.0

--- CV/home_page/urls.py
+++ CV/home_page/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('home_page/hobby_page/', views.hobby_page, name='hobby_page'),
     path('home_page/qualities_page/', views.qualities_page, name='qualities_page'),
     path('home_page/exp_page/hobby_page/', views.hobby_page, name='hobby_page'),
-    path('home_page/exp_page/qualities_page/', views.qualities_page, name='qualities_page')
+    path('home_page/exp_page/qualities_page/', views.qualities_page, name='qualities_page'),
+    path('home_page/qualities_page/hobby_page/', views.hobby_page, name='hobby_page'),
+    path('home_page/qualities_page/exp_page/', views.exp_page, name='exp_page'),
+    path('home_page/hobby_page/qualities_page/', views.qualities_page, name='qualities_page'),
+    path('home_page/hobby_page/exp_page/', views.exp_page, name='exp_page'),
 ]
