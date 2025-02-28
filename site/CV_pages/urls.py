3938fbc3e99350cda437d56e5962a6648fec9c19
Hubert-Gasior <164912381+Hubert-Gasior@users.noreply.github.com>
2025-02-28 15:15:40 +0100

    Add files via upload
    static/templates/settings

---
+++ site/CV_pages/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [ 
+    #path('test_page/', views.test_page, name='test_page'),
+    path('home_page/', views.home_page, name='home_page'),
+    path('home_page/exp_page/', views.exp_page, name='exp_page'),
+    path('home_page/hobby_page/', views.hobby_page, name='hobby_page'),
+    path('home_page/qualities_page/', views.qualities_page, name='qualities_page'),
+    path('home_page/exp_page/hobby_page/', views.hobby_page, name='hobby_page'),
+    path('home_page/exp_page/qualities_page/', views.qualities_page, name='qualities_page'),
+    path('home_page/qualities_page/hobby_page/', views.hobby_page, name='hobby_page'),
+    path('home_page/qualities_page/exp_page/', views.exp_page, name='exp_page'),
+    path('home_page/hobby_page/qualities_page/', views.qualities_page, name='qualities_page'),
+    path('home_page/hobby_page/exp_page/', views.exp_page, name='exp_page'),
+]
---
+++ site/CV_pages/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def home_page(request):
+    template = loader.get_template('home_page.html')
+    return HttpResponse(template.render())
+
+def exp_page(request):
+    template = loader.get_template('exp_page.html')
+    return HttpResponse(template.render())
+
+def hobby_page(request):
+    template = loader.get_template('hobby_page.html')
+    return HttpResponse(template.render())
+
+def qualities_page(request):
+    template = loader.get_template('qualities_page.html')
+    return HttpResponse(template.render())
+
+#def test_page(request):
+#    template = loader.get_template('test_page.html')
+#    return HttpResponse(template.render())
